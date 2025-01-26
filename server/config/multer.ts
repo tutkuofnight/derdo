2176e71ffbb446f67d532b185a84be3d52049aa2
Tutku Ucan <mettheus0@gmail.com>
2025-01-26 18:00:49 +0300

    add track image

--- server/config/multer.ts
+++ server/config/multer.ts
@@ -1,25 +1,71 @@
 import multer, { StorageEngine } from "multer"
 import path from "node:path"
-import { v4 as uuidv4 } from "uuid"
 import { Request } from "express"
 
-const storage: StorageEngine = multer.diskStorage({
+const acceptImageFile = ["png", "jpeg", "jpg", "gif" , "webp"]
+const acceptAudioFile = ["mpeg", "mp3"]
+
+const fileInfo = (file: any) => {
+  const fileName = file.originalname.split(" ").join("-")
+  const fileType = fileName.split(".").at(-1)
+  return { fileName, fileType }
+}
+
+const trackStorage: StorageEngine = multer.diskStorage({
   destination: (req: Request, file: any, cb: any) => {
-    console.log(req.body)
-    cb(null, path.join(__dirname, "../public/uploads/"))
+    if (file.fieldname === 'image') {
+      cb(null, path.join(__dirname, "../public/uploads/track/image"))
+    } else if (file.fieldname === 'track') {
+      cb(null, path.join(__dirname, "../public/uploads/track"))
+    }
   },
   filename: (req: Request, file: any, cb: any) => {
-    const id = uuidv4()
-    let fileName = file.originalname.split(" ").join("-")
+    const { fileType } = fileInfo(file)
+    let fileName: string
 
-    if (fileName.endsWith(".mp3")) {
-      fileName = `${id}.mp3`
+    if (file.fieldname === 'image' && acceptImageFile.includes(fileType!)) {
+      fileName = `${req.params.id}${path.extname(file.originalname)}`
+    } else if (file.fieldname === 'song' && acceptAudioFile.includes(fileType!)) {
+      fileName = `${req.params.id}.mp3`
     }
-
-    req.body.id = id
-    cb(null, fileName)
+    console.log("calisiyor")
+    cb(null, fileName!)
   },
 })
 
-const upload = multer({ storage })
-export default upload
+const trackImageStorage: StorageEngine = multer.diskStorage({
+  destination: (req: Request, file: any, cb: any) => {
+    cb(null, path.join(__dirname, "../public/uploads/track/image"))
+  },
+  filename: (req: Request, file: any, cb: any) => {
+    const { fileType } = fileInfo(file)
+    let fileName: string
+    if (acceptImageFile.includes(fileType!)) {
+      fileName = `${req.params.id}.${fileType}`
+    }
+    cb(null, fileName!)
+  },
+})
+
+// const userImageStorage:StorageEngine = multer.diskStorage({
+//   destination: (req: Request, file: any, cb: any) => {
+//     console.log(req.body)
+//     cb(null, path.join(__dirname, "../public/uploads/user/image"))
+//   },
+//   filename: (req: Request, file: any, cb: any) => {
+//     const id = uuidv4()
+//     let fileName = file.originalname.split(" ").join("-")
+
+//     if (fileName.endsWith(".mp3")) {
+//       fileName = `${id}.mp3`
+//     }
+
+//     req.body.id = id
+//     cb(null, fileName)
+//   },
+// })
+// export const userImageUpload = multer({ storage: userImageStorage })
+
+export const trackUpload = multer({ storage: trackStorage })
+export const trackImageUpload = multer({ storage: trackImageStorage })
+
--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,16 @@
-import express, { Request, Response } from "express";
-import { createServer } from "node:http";
-import { Server } from "socket.io";
-import cors from "cors";
-import bodyParser from "body-parser";
-import path from "node:path";
-import upload from "./config/multer";
-import dotenv from "dotenv";
+import express, { Request, Response } from "express"
+import { createServer } from "node:http"
+import { Server } from "socket.io"
+import cors from "cors"
+import bodyParser from "body-parser"
+import path from "node:path"
+import dotenv from "dotenv"
 import { createClient } from "redis"
 
 dotenv.config();
 
 const app = express();
-const server = createServer(app);
+const server = createServer(app)
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
@@ -19,59 +18,59 @@ const io = new Server(server, {
     allowedHeaders: ["Content-Type"],
     credentials: true,
   },
-});
+})
 const client = createClient({ url: process.env.REDIS_URL })
 .on('error', err => console.log('Redis Client Error', err))
-.connect();
+.connect()
 
 // Middleware
-app.use(express.json());
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
+app.use(bodyParser.json({ limit: "50mb" }))
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")))
 app.use(cors({
   origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
+import { trackUpload, trackImageUpload } from "./config/multer"
+
 // Routes
 app.get("/", (req: Request, res: Response) => {
   return res.status(200).json({
     message: "hi, rewind!",
-  });
-});
+  })
+})
 
-app.post("/track/upload", upload.single("song") as any, (req: Request, res: Response) => {
+app.post("/track/upload/:id", trackUpload.fields([{ name: "song" }, { name: "image" }]), (req: Request, res: Response) => {
   return res.status(200).json({
     success: true,
-    id: req.body.id,
-  });
-});
+  })
+})
 
 // Socket.IO Events
 io.on("connection", (socket) => {
   socket.on("join-room", ({ data }: { data: { room: string } }) => {
-    socket.join(data.room);
-    console.log("User joined the room:", data.room);
-    io.to(data.room).emit("joinedUser", { data });
-  });
+    socket.join(data.room)
+    console.log("User joined the room:", data.room)
+    io.to(data.room).emit("joinedUser", { data })
+  })
 
   socket.on("set", (data: any, room: string) => {
-    socket.to(room).emit("set", data);
-  });
+    socket.to(room).emit("set", data)
+  })
 
   socket.on("play", (state: any, room: string) => {
-    socket.to(room).emit("play", state);
-  });
+    socket.to(room).emit("play", state)
+  })
 
   socket.on("pause", (state: any, room: string) => {
-    socket.to(room).emit("pause", state);
-  });
+    socket.to(room).emit("pause", state)
+  })
 
   socket.on("timeSeeked", (duration: number, room: string) => {
-    socket.to(room).emit("timeSeeked", duration);
-  });
+    socket.to(room).emit("timeSeeked", duration)
+  })
 
   // Debugging: Check rooms and clients
   // io.of('/').adapter.rooms.forEach((clients, room) => {
@@ -82,5 +81,5 @@ io.on("connection", (socket) => {
 // Start Server
 const port = process.env.PORT
 server.listen(port, () => {
-  console.log(`WebSocket server running on ${port}`);
-});
+  console.log(`WebSocket server running on ${port}`)
+})
